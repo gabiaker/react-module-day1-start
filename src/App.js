09b4363cb6f3d18f064b46b85de627562177ac74
John Doe <gabiak+stack@gmail.com>
2020-07-11 13:33:30 +0200

    Components and Props

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+class App extends Component {
+ 
+  render() {
+ 
+    const formatName = (user) => {
+      return `${user.firstName} ${user.lastName}`;
+    };
+   
+    const user = {
+      firstName: 'Harper',
+      lastName: 'Perez',
+      avatarUrl: ''
+    };
+    
+    const element = (
+      <h2>
+        Hello, {formatName(user)}!
+      </h2>
+    );
+ 
+    return (
+      <div className="App">
+        <h1> Hello Ironhackers! </h1>
+        {element}
+      </div>
+    );
+  }
+}
+ 
+export default App;
